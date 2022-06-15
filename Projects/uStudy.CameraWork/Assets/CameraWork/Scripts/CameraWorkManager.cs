01b314a0957e76417284479fe7e529dbcce67b60
jtfrom9 <jun.tachikawa@gmail.com>
2022-06-15 23:17:18 +0900

    add camera work using DOTween

--- Projects/uStudy.CameraWork/Assets/CameraWork/Scripts/CameraWorkManager.cs
+++ Projects/uStudy.CameraWork/Assets/CameraWork/Scripts/CameraWorkManager.cs
@@ -1,13 +1,15 @@
 using System;
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using DG.Tweening;
 
 public class CameraWorkManager : MonoBehaviour
 {
-    Transform target;
+    AgentController current;
+
     AgentController[] agents;
 
     [SerializeField]
@@ -15,38 +17,78 @@ public class CameraWorkManager : MonoBehaviour
 
     [SerializeField]
     Dropdown dropdown;
-
-    void SetTarget(AgentController agent) {
-        var target = agent.gameObject.transform;
-        if(this.target != target) {
-            this.target = target;
-            _camera.SetParent(this.target, false);
-            _camera.localPosition = new Vector3(0, 1, -3);
-            _camera.localRotation = Quaternion.identity;
-
-            dropdown.value = Array.IndexOf(agents, agent);
-        }
-    }
+    [SerializeField]
+    Button prevButton;
+    [SerializeField]
+    Button nextButton;
+    [SerializeField]
+    Toggle birdCamToggle;
 
     [SerializeField]
-    AgentController _agent;
+    Boolean birdCam = false;
+
+    void SetTarget(AgentController agent)
+    {
+        var move = Vector3.zero;
+        var rot = Vector3.zero;
+        if (birdCam)
+        {
+            move = new Vector3(0, 15, -5);
+            rot = new Vector3(60, 0, 0);
+        }
+        else
+        {
+            move = new Vector3(0, 1, -3);
+            rot = Vector3.zero;
+        }
+
+        this.current = agent;
+
+        // _camera.SetParent(this.target, false);
+        // _camera.localPosition = new Vector3(0, 1, -3);
+        // _camera.localRotation = Quaternion.identity;
+
+        _camera.SetParent(this.current.transform);
+        if (DOTween.IsTweening(_camera))
+        {
+            _camera.DOKill();
+        }
+        _camera.DOLocalMove(move, 1);
+        _camera.DOLocalRotate(rot, 1);
+
+        dropdown.value = Array.IndexOf(agents, agent);
+    }
 
     void Start() {
         agents = FindObjectsOfType<AgentController>();
-        foreach (var agent in agents) {
-            Debug.Log($"agent: {agent.agentName}");
+        foreach (var agent in agents.OrderBy(agent => agent.agentName)) {
             dropdown.AddOptions(new List<string>() { agent.agentName });
         }
 
         dropdown.OnValueChangedAsObservable().Subscribe(index =>
         {
             SetTarget(agents[index]);
-        });
+        }).AddTo(this);
 
-
-        if (_agent)
+        nextButton.OnClickAsObservable().Subscribe(_ =>
         {
-            SetTarget(_agent);
-        }
+            var index = (current == null) ? -1 : Array.IndexOf(agents, current);
+            var next = index != agents.Length - 1 ? index + 1 : 0;
+            Debug.Log($"{index},{next}");
+            SetTarget(agents[next]);
+        }).AddTo(this);
+
+        prevButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            var index = (current == null) ? -1 : Array.IndexOf(agents, current);
+            var prev = index != 0 ? index - 1 : agents.Length - 1;
+            SetTarget(agents[prev]);
+        }).AddTo(this);
+
+        birdCamToggle.OnValueChangedAsObservable().Subscribe(v =>
+        {
+            this.birdCam = v;
+            SetTarget(current);
+        }).AddTo(this);
     }
 }
