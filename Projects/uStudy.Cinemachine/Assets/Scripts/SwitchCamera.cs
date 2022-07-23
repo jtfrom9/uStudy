438515123c317c741318918087895d12fa99b35d
jtfrom9 <jun.tachikawa@gmail.com>
2022-07-23 16:45:49 +0900

    add Tower camera

--- Projects/uStudy.Cinemachine/Assets/Scripts/SwitchCamera.cs
+++ Projects/uStudy.Cinemachine/Assets/Scripts/SwitchCamera.cs
@@ -9,28 +9,70 @@ public class SwitchCamera : MonoBehaviour
     CinemachineVirtualCamera trackingVCamera;
     [SerializeField]
     CinemachineVirtualCamera topDownVCamera;
+    [SerializeField]
+    CinemachineVirtualCamera towerVCamera;
 
-    bool tracking = false;
+
+    public enum CameraType
+    {
+        Tracking,
+        TopDown,
+        Tower
+    };
+
+    [SerializeField]
+    CameraType cameraType = CameraType.Tracking;
+
+    void setPriority()
+    {
+        switch (cameraType)
+        {
+            case CameraType.Tracking:
+                trackingVCamera.Priority = 11;
+                topDownVCamera.Priority = 10;
+                towerVCamera.Priority = 10;
+                break;
+            case CameraType.TopDown:
+                trackingVCamera.Priority = 10;
+                topDownVCamera.Priority = 11;
+                towerVCamera.Priority = 10;
+                break;
+            case CameraType.Tower:
+                trackingVCamera.Priority = 10;
+                topDownVCamera.Priority = 10;
+                towerVCamera.Priority = 11;
+                break;
+        }
+    }
+
+    void switchCameraType()
+    {
+        switch (cameraType) {
+            case CameraType.Tracking:
+                cameraType = CameraType.TopDown;
+                break;
+            case CameraType.TopDown:
+                cameraType = CameraType.Tower;
+                break;
+            case CameraType.Tower:
+                cameraType = CameraType.Tracking;
+                break;
+        }
+        setPriority();
+    }
 
     void Start()
     {
         if(trackingVCamera==null || topDownVCamera==null) {
             throw new System.Exception("invalid");
         }
-        tracking = trackingVCamera.Priority > topDownVCamera.Priority;
+        setPriority();
     }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)) {
-            if (tracking)
-            {
-                trackingVCamera.Priority -= 2;
-                tracking = false;
-            } else {
-                trackingVCamera.Priority += 2;
-                tracking = true;
-            }
+            switchCameraType();
         }
     }
 }
