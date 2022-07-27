635d2c312f0b6c2ed74c4fdfe9e91e9e61ab841e
jtfrom9 <jun.tachikawa@gmail.com>
2022-07-27 12:13:50 +0900

    add embeded packages

---
+++ Projects/uStudy.LocalPackage/Assets/Lib/Lib.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Lib
+{
+    public static void Foo() {
+        Debug.Log("Lib.Foo");
+    }
+}
---
+++ Projects/uStudy.LocalPackage/Packages/com.jtfrom9.test.package/Runtime/Hoge.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public static class Hoge 
+{
+    public static void Foo() {
+        Debug.Log("Fooe");
+    }
+}
